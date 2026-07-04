901856e67e3357a0ca77227dfe58fd10715f9674
shayan090714 <kamini_s29@yahoo.com>
2026-07-04 10:45:58 +0000

    battle

--- DSA_Projects/Battle.py
+++ DSA_Projects/Battle.py
@@ -19,12 +19,6 @@ while True:
             print("You deal",p_damage,"damage to the enemy!")
             ehp = ehp - p_damage
             print("Enemy's health is",ehp)
-            if ehp <= 0:
-                print("You win!")
-                print("You completed level",level)
-                level = level + 1
-                print("Level",level,"reached!")
-                break
             input("Press Enter to continue...")
         elif p_attack == "Super Attack":
             if Super_Attack_Used:
@@ -37,12 +31,6 @@ while True:
                 print("You deal",p_damage,"damage to the enemy!")
                 ehp = ehp - p_damage
                 print("Enemy's health is",ehp)
-                if ehp <=0:
-                    print("You win!")
-                    print("You completed level",level)
-                    level = level + 1
-                    print("Level",level,"reached!")
-                    break
                 input("Press Enter to continue...")
         elif p_attack == "Heal":
             php = php + 10
@@ -53,12 +41,16 @@ while True:
             input("Press Enter to continue...")
         elif p_attack == "Run":
             print("You run!")
-            ran_away = True
             exit()
         else:
             print("Invalid choice!")
-            continue
-        if not php > 0 and ehp > 0:
+        if ehp <=0:
+            print("You win!")
+            print("You completed level",level)
+            level = level + 1
+            print("Level",level,"reached!")
+            break
+        if php > 0 and ehp > 0:
             enemy_attack = random.choice(e_attack)
             if enemy_attack == "Bite":
                 e_damage = random.randint(5 + level, 10 + level)
@@ -66,50 +58,30 @@ while True:
                 print("Enemy deals",e_damage,"damage to you!")
                 php = php - e_damage
                 print("Your health is",php)
-                if php <= 0:
-                    print("You lose!")
-                    print("You lost at level",level)
-                    score = level
-                    if score > high_score:
-                        high_score = score
-                        print("Your new high score is",score,".Congratulations!")
-                    else:
-                        print("Your score is",score,".Better luck next time!")
-                    exit()
             elif enemy_attack == "Smash":
                     e_damage = random.randint(8 + level, 13 + level)
                     print("Enemy chose Smash!")
                     print("Enemy deals",e_damage,"damage to you!")
                     php = php - e_damage
                     print("Your health is",php)
-                    if php <= 0:
-                        print("You lose!")
-                        print("You lost at level",level)
-                        score = level
-                        if score > high_score:
-                            high_score = score
-                            print("Your new high score is",score,".Congratulations!")
-                        else:
-                            print("Your score is",score,".Better luck next time!")
-                        exit()
             elif enemy_attack == "Roar":
                 print("Enemy chose Roar!")
                 print("Its so loud it deals 5 damage to you!")
                 php = php - 5
                 print("Your health is",php)
-                if php <= 0:
-                        print("You lose!")
-                        print("You lost at level",level)
-                        score = level
-                        if score > high_score:
-                            high_score = score
-                            print("Your new high score is",score,".Congratulations!")
-                        else:
-                            print("Your score is",score,".Better luck next time!")
-                        exit()
             elif enemy_attack == "Heal":
                 print("Enemy chose Heal!")
                 print("Enemy's health increases by 10!")
                 ehp = ehp + 10
                 print("Enemy's health is",ehp)
                 continue
+            if php <= 0:
+                print("You lose!")
+                print("You lost at level",level)
+                score = level
+                if score > high_score:
+                    high_score = score
+                    print("Your new high score is",score,".Congratulations!")
+                else:
+                    print("Your score is",score,".Better luck next time!")
+                exit()
